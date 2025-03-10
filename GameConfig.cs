9ba8bce9f1176cd24369317a83590ad15154da65
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-03-10 09:48:39 -0300

    Add GameConfig

---
+++ GameConfig.cs
@@ -0,0 +1,6 @@
+public class GameConfig
+{
+    public int Volume { get; set; } = 50;
+    public string Resolution { get; set; } = "1920x1080";
+    public string Difficulty { get; set; } = "Médio";
+}
